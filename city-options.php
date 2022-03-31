1fb901eda6be6b9a2a5cc9d21448b6de3059499e
ptlnisarg31 <techngyanyt@gmail.com>
2022-03-31 15:47:35 +0530

    nothing
    Co-authored-by: Dev Patel <DevPatel04@users.noreply.github.com>

--- city-options.php
+++ city-options.php
@@ -5,7 +5,6 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!-- Important to make website responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Choose city - Minute maintanunce</title>
 
@@ -21,7 +20,7 @@ session_start();
    <?php
    include 'include/header.php';
    ?>
-    <!-- Navbar Section Ends Here -->
+   
 
     
 	<?php
